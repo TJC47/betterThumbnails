dfcfa1906ea676931958b0f3e68efbf11c67aabb
DumbCaveSpider <markiel_giles@outlook.com>
2025-07-25 00:03:30 +0800

    tidy things up :)

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
 
 using namespace geode::prelude;
 
-
 EventListener<web::WebTask> m_listener;
 
 class $modify(MyCreatorLayer, CreatorLayer)
@@ -39,18 +38,35 @@ class $modify(MyCreatorLayer, CreatorLayer)
 
 	void onMyButton(CCObject *)
 	{
-		if (Mod::get()->hasSavedValue("token")) {
+		if (Mod::get()->hasSavedValue("token"))
+		{
 			CCDirector::get()->pushScene(CCTransitionFade::create(.5f, BetterThumbnailLayer::scene()));
 		}
-		else {
+		else
+		{
 
-        createQuickPopup("Notice","To use the Better Level Thumbnails Mod you must <cg>authenticate</c> with your Geometry Dash account and the Level Thumbnails servers.","No","Authenticate",[this](auto alert,bool btn2){
+			createQuickPopup("Notice", "To use the Better Level Thumbnails Mod you must <cg>authenticate</c> with your Geometry Dash account and the Level Thumbnails servers.", "No", "Authenticate", [this](auto alert, bool btn2)
+							 {
             if (btn2){
 				auto notif = NotificationUI::create("Please Hold", "Authenticating with argon...");
 				if (notif)
 				{
 					this->addChild(notif, 100);
 				}
+
+				// loading background
+				auto winSize = CCDirector::sharedDirector()->getWinSize();
+				auto bg = CCLayerColor::create({0, 0, 0, 200});
+				bg->setPosition({0, 0});
+				this->addChild(bg, 99);
+
+				auto loadingLabel = CCLabelBMFont::create("Authenticating with Argon...", "bigFont.fnt");
+				loadingLabel->setAnchorPoint({0.5f, 0.5f});
+				loadingLabel->setPosition({winSize.width / 2.f, winSize.height / 2.f - 30.f});
+				loadingLabel->setScale(0.5f);
+				this->addChild(loadingLabel, 100);
+
+				// start the argon auth process
                 auto res = argon::startAuth([this](Result<std::string> res){
                     if (!res){
                         FLAlertLayer::create("Oops!","The <cy>Argon</c> auth process failed.","OK")->show();
@@ -58,8 +74,6 @@ class $modify(MyCreatorLayer, CreatorLayer)
                     }
 					auto argon_token = res.unwrap();
 					Mod::get()->setSavedValue<std::string>("token", argon_token);
-
-
 
 					auto req = web::WebRequest();
 
@@ -91,13 +105,13 @@ class $modify(MyCreatorLayer, CreatorLayer)
 							Mod::get()->setSavedValue<std::string>("token", token);
 							Mod::get()->setSavedValue<std::string>("role", role);
 							Mod::get()->setSavedValue<long>("user_id", id);
+
 							CCDirector::get()->pushScene(CCTransitionFade::create(.5f, BetterThumbnailLayer::scene()));
 						}
 					});
 					m_listener.setFilter(task);
                 });
-            }
-        });
+            } });
 		}
 	}
 };
