58c6a4efc3917fbfd3f02ad1cab23ff9bff68e17
DumbCaveSpider <markiel_giles@outlook.com>
2025-09-06 21:32:14 +0800

    fancy thumbnaillayer

--- src/node/ThumbnailNode.hpp
+++ src/node/ThumbnailNode.hpp
@@ -2,6 +2,7 @@
 #include <cocos2d.h>
 #include <cocos-ext.h>
 #include <Geode/Geode.hpp>
+#include <string>
 
 using namespace geode::prelude;
 
@@ -9,7 +10,14 @@ class ThumbnailNode : public cocos2d::CCNode
 {
 public:
     EventListener<web::WebTask> m_listener;
+    // Stored data from API for opening the info layer
     int m_thumbId = 0;
+    int m_userId = 0;
+    std::string m_username;
+    int m_levelId = 0;
+    bool m_accepted = false;
+    std::string m_uploadTime;
+    bool m_replacement = false;
     static ThumbnailNode *create(const cocos2d::CCSize &size, int id, int user_id, const std::string &username, int level_id, bool accepted, const std::string &upload_time, bool replacement);
     bool init(const cocos2d::CCSize &size, int id, int user_id, const std::string &username, int level_id, bool accepted, const std::string &upload_time, bool replacement);
     void onViewButton(CCObject*);
